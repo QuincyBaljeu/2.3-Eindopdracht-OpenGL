c5981d6f7bd7409b98d59ea6973047458136681b
Quincy <q.baljeu@gmail.com>
2020-06-04 09:58:00 +0200

    [ADD] Added fps cam

--- TI2.4-CGR-Template/main.cpp
+++ TI2.4-CGR-Template/main.cpp
@@ -1,18 +1,27 @@
+//header files
+#include "tigl.h"
+#include "FpsCam.h"
+
+//lib files
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include "tigl.h"
 #include <glm/gtc/matrix_transform.hpp>
+
+//misc.
 using tigl::Vertex;
 
 #pragma comment(lib, "glfw3.lib")
 #pragma comment(lib, "glew32s.lib")
 #pragma comment(lib, "opengl32.lib")
 
-GLFWwindow* window;
-
+//prototypes
 void init();
 void update();
 void draw();
+
+//Attributes
+GLFWwindow* window;
+double lastFrameTime;
 
 int main(void)
 {
@@ -44,6 +53,7 @@ int main(void)
     return 0;
 }
 
+FpsCam* camera;
 
 void init()
 {
@@ -52,24 +62,51 @@ void init()
         if (key == GLFW_KEY_ESCAPE)
             glfwSetWindowShouldClose(window, true);
     });
-
+    camera = new FpsCam(window);
 }
-
 
 void update()
 {
-
+    double currentTime = glfwGetTime();
+    double deltaTime = currentTime - lastFrameTime;
+    lastFrameTime = currentTime;
+    camera->update(window);
 }
 
 void draw()
 {
+    //settings
     glClearColor(0.3f, 0.4f, 0.6f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    int viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    glm::mat4 projection = glm::perspective(glm::radians(75.0f), viewport[2] / (float)viewport[3], 0.01f, 100.0f);
+
+    tigl::shader->setProjectionMatrix(projection);
+    tigl::shader->setViewMatrix(camera->getMatrix());
+    tigl::shader->setModelMatrix(glm::mat4(1.0f));
+
+    tigl::shader->enableColor(true);
+
+    glEnable(GL_DEPTH_TEST);
     
+    //drawing
 
     tigl::begin(GL_TRIANGLES);
-    tigl::addVertex(Vertex::PC(glm::vec3(-1, 0, 0), glm::vec4(1, 0, 0, 1)));
-    tigl::addVertex(Vertex::PC(glm::vec3(1, 0, 0), glm::vec4(0, 1, 0, 1)));
-    tigl::addVertex(Vertex::PC(glm::vec3(0, 1, 0), glm::vec4(0, 0, 1, 1)));
+    tigl::addVertex(Vertex::PC(glm::vec3(-2, -1, -4), glm::vec4(1, 0, 0, 1)));
+    tigl::addVertex(Vertex::PC(glm::vec3(2, -1, -4), glm::vec4(0, 1, 0, 1)));
+    tigl::addVertex(Vertex::PC(glm::vec3(0, 1, -4), glm::vec4(0, 0, 1, 1)));
+
+
+    tigl::addVertex(Vertex::PC(glm::vec3(-10, -1, -10), glm::vec4(1, 1, 1, 1)));
+    tigl::addVertex(Vertex::PC(glm::vec3(-10, -1, 10), glm::vec4(1, 1, 1, 1)));
+    tigl::addVertex(Vertex::PC(glm::vec3(10, -1, 10), glm::vec4(1, 1, 1, 1)));
+
+    tigl::addVertex(Vertex::PC(glm::vec3(-10, -1, -10), glm::vec4(1, 1, 1, 1)));
+    tigl::addVertex(Vertex::PC(glm::vec3(10, -1, -10), glm::vec4(1, 1, 1, 1)));
+    tigl::addVertex(Vertex::PC(glm::vec3(10, -1, 10), glm::vec4(1, 1, 1, 1)));
+
     tigl::end();
+   
 }
